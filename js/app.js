ca75c6496466723759f87e2e275c59fa878bb2de
Carl Jerrit Aquino <jerritt.aquino@gmail.com>
2018-11-04 14:56:10 -0500

    fix comments and add enemy instances

--- js/app.js
+++ js/app.js
@@ -25,17 +25,12 @@
 //Convert provided enemy class to ES6
 class Enemy {
   constructor(x, y){
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.x = x;
     this.y = y;
     this.sprite = 'images/enemy-bug.png';
   }
 
-  // Update the enemy's position, required method for game
+  // Update the enemy's position
   // Parameter: dt, a time delta between ticks
   update(dt) {
     // You should multiply any movement by the dt parameter
@@ -49,14 +44,6 @@ class Enemy {
   }
 }
 
-
-
-
-
-
-
-
-// Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
@@ -69,11 +56,8 @@ class Player {
   }
 
   // Update the player's position
-  // Parameter: dt, a time delta between ticks
   update() {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
   };
 
   // Draw the player on the screen
@@ -87,14 +71,12 @@ class Player {
 }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate objects.
 const player = new Player();
 const enemy1 = new Enemy(0,55);
-// const enemy2 = new Enemy();
-// const enemy3 = new Enemy();
-const allEnemies = [enemy1];
+const enemy2 = new Enemy(0,140);
+const enemy3 = new Enemy(0,225);
+const allEnemies = [enemy1,enemy2,enemy3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
